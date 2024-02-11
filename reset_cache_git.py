4615a41722901f9b437a0bd6644e4dece41f5bb2
Cipher58 <jackfranklin899@gmail.com>
2024-02-11 02:56:47 +0000

    Automation

--- reset_cache_git.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system("git rm -rf --cached .")
-os.system("git add .")
-os.system("python3 upload.py")
--- upload.py
+++ upload.py
@@ -1,5 +1,8 @@
 import os
 
+os.system("git rm -rf --cached .")
+os.system("git add .")
+
 os.system("git add *")
 os.system("git commit -m 'Automation'")
 os.system("git push")
