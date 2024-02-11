f179dedcd59c931e5c77fb9b7d2e94510abd9a58
Cipher58 <jackfranklin899@gmail.com>
2024-02-11 01:59:20 +0000

    Automation

--- main.py
+++ main.py
@@ -2,23 +2,29 @@
 # Built-in Python libraries
 import os
 import time
+import requests
 
 # Internal directory libraries
 from Utilities.functions import *
 from Utilities.record import *
 from Utilities.transcribe import *
+from server import *
 
+# Config (tbimported to json)
 
-def classify(text):
-    os.system(f"./AI/classifier.out '{text}'")
-    with open("short_term_memory/current_class.json", "r") as f:
-        intent_class = json.load(f)
-    time.sleep(1)
-    os.system(f"./AI/choose_response.out '{text}'")
-    DoFunction(intent_class)
+port = 8000
+
+def classify(sentence):
+    os.system(f"./Utilities/send_request http://localhost:{port} '{sentence}'")
+
     with open("./short_term_memory/output.txt", "r") as f:
         output = f.read()
-        os.system(f"./Utilities/tts '{output}'")
+    
+    with open("./short_term_memory/current_class.json", "r") as f:
+        intent_class = json.load(f)
+    
+    os.system(f"./Utilities/tts '{output}'")
+    DoFunction(intent_class)
 
 def audio_command():
     Input = Audio()
@@ -32,6 +38,12 @@ def audio_command():
         except:
             pass
 
+def server():
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print(f"{port}")
+    httpd.serve_forever()
+
 def text_command():
     text = input("You: ")
     classify(text)
@@ -40,5 +52,7 @@ if __name__ == "__main__":
     mode = input("Mode: ")
     if mode.lower() == "audio":
         audio_command()
+    elif mode.lower() == "server":
+        server()
     else:
         text_command()
--- server.py
+++ server.py
@@ -1,43 +1,31 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from io import BytesIO
 import json
-from main import *
-
-Jarvis = JarvisAI()
+import os
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        # get the content length of the post request
         content_length = int(self.headers['Content-Length'])
-
-        # read the post data
         post_data = self.rfile.read(content_length)
-
-        # decode the post data from bytes to string
         post_data = post_data.decode('utf-8')
-
-        # parse the post data as a JSON object
         post_data = json.loads(post_data)
-
-        # get the message text from the JSON object
         message_text = post_data['message']
-
-        # send the message text to your chatbot and get the response
         sentence = str(message_text)
+        os.system(f"./AI/classifier.out '{sentence}'")
+        os.system(f"./AI/choose_response.out '{sentence}'")
 
-        ResponseOutput = Jarvis.say(sentence)
         with open("short_term_memory/current_class.json", "r") as file:
             intent_class = json.load(file)
-
-        # Create a response JSON object
+        
+        with open("short_term_memory/output.txt", "r") as output:
+            ResponseOutput = output.read()
+        
         response_data = {
             'response': ResponseOutput,
             'intent_class': intent_class
         }
 
-        # Convert the response JSON object to a JSON string
         response_json = json.dumps(response_data)
-
         response_bytes = response_json.encode('utf-8')
 
         self.send_response(200)
