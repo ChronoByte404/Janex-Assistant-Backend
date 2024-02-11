9d19a4f7876d916230a5d06b2d92f3b7c0c97f61
Cipher58 <jackfranklin899@gmail.com>
2024-02-11 04:15:29 +0000

    Automation

--- download.py
+++
@@ -1,7 +0,0 @@
-import os
-
-try:
-    os.system("git pull")
-except:
-    os.system("git stash")
-    os.system("git pull")
--- upload.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.system("git rm -rf --cached .")
-os.system("git add .")
-
-os.system("git add *")
-os.system("git commit -m 'Automation'")
-os.system("git push")
