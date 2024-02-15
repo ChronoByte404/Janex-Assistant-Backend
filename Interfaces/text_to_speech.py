5f7733b9c7cc495e3742411122e1471a438699a9
Cipher58 <jackfranklin899@gmail.com>
2024-02-15 22:03:47 +0000

    Automation

--- Interfaces/text_to_speech.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class TTS:
-    def say(text):
-        os.system(f"'./Scripts/speech.sh' '{text}'")
-
-if __name__ == "__main__":
-    Voice = TTS()
-    Voice.say("Hello, world. I am professor yana.")
